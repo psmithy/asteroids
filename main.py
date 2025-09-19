585c716abd3cbebe7ebc282fddf90446269110c1
psmithy <paulsmith1044@gmail.com>
2025-09-19 17:39:31 -0400

    add clock, add player rendering

--- main.py
+++ main.py
@@ -1,22 +1,29 @@
 import pygame
 from constants import *
+from player import Player
 
 pygame.init()
 screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+clock = pygame.time.Clock()
+dt = 0
 
 def main():
     print("Starting Asteroids!")
     print(f"Screen width: {SCREEN_WIDTH}")
     print(f"Screen height: {SCREEN_HEIGHT}")
 
+    player = Player((SCREEN_WIDTH / 2), (SCREEN_HEIGHT / 2), PLAYER_RADIUS)
+
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
 
-    screen.fill([0, 0, 0])
-    pygame.display.flip()
+        screen.fill([0, 0, 0])
+        player.draw(screen)
+        pygame.display.flip()
 
+        dt = clock.tick(60) / 1000
 
 if __name__ == "__main__":
     main()
