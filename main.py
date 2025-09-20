f74e6475ddfe8a1c8804223465d546f944347fa5
psmithy <paulsmith1044@gmail.com>
2025-09-20 17:37:23 -0400

    restructured drawing

--- main.py
+++ main.py
@@ -6,11 +6,15 @@ pygame.init()
 screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 clock = pygame.time.Clock()
 
+
 def main():
     print("Starting Asteroids!")
     print(f"Screen width: {SCREEN_WIDTH}")
     print(f"Screen height: {SCREEN_HEIGHT}")
 
+    updatable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
+    Player.containers = (updatable, drawable)
     player = Player((SCREEN_WIDTH / 2), (SCREEN_HEIGHT / 2), PLAYER_RADIUS)
     dt = 0
 
@@ -19,9 +23,12 @@ def main():
             if event.type == pygame.QUIT:
                 return
 
-        player.update(dt)
+        updatable.update(dt)
         screen.fill([0, 0, 0])
-        player.draw(screen)
+
+        for i in drawable:
+            i.draw(screen)
+
         pygame.display.flip()
 
         dt = clock.tick(60) / 1000
